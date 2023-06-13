74464d106c73f08c39138f6161cb9f9ba817002e
julianroldanjj <136298517+julianroldanjj@users.noreply.github.com>
2023-06-13 18:43:43 -0500

    Update spaceship.py
    cambios 13/06/2023

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,19 +1,9 @@
-import pygame
-from pygame.sprite import Sprite
+import pygame.sprite
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
 
-from game.utils.constants import SPACESHIP
-
-# casi Todo en pygame es un objeto
-# Un personaje en mi juego es un objeto (instancia de algo)
-# La nave (spaceship) es un personaje => necesito una clase
-
-
-# SpaceShip es una clase derivada (hija) de Sprite
-
-# spaceship tiene una "imagen"
-class SpaceShip(Sprite):
-    
+class SpaceShip(pygame.sprite.Sprite):
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.image_size = (40, 60)
         self.image = pygame.transform.scale(SPACESHIP, self.image_size)
         self.image_rect = self.image.get_rect()
@@ -21,6 +11,21 @@ class SpaceShip(Sprite):
         self.image_rect.y = self.image_size[1]
 
     def update(self):
-        pass
-
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:  # Tecla A o Flecha izquierda para moverse a la izquierda
+            self.image_rect.x -= 7
+        if self.image_rect.right < 0:
+            self.image_rect.left = SCREEN_WIDTH
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:  # Tecla D o Flecha derecha para moverse a la derecha
+            self.image_rect.x += 7
+        if self.image_rect.left > SCREEN_WIDTH:
+            self.image_rect.right = 0
+        if keys[pygame.K_w] or keys[pygame.K_UP]:  # Tecla W o Flecha arriba para moverse hacia arriba
+            self.image_rect.y -= 7
+        if self.image_rect.top < 0:
+            self.image_rect.top = 0
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:  # Tecla S o Flecha abajo para moverse hacia abajo
+            self.image_rect.y += 7
+        if self.image_rect.bottom > SCREEN_HEIGHT:
+            self.image_rect.bottom = SCREEN_HEIGHT
 
